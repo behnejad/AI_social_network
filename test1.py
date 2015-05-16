469197685832a0dd7284dfbc5fa07a24002baf3b
Hooman Behnejad <hooman@mail.iredmail.org>
2015-05-16 15:52:44 +0430

    first test

--- test1.py
+++ test1.py
@@ -5,10 +5,6 @@ from sklearn import linear_model
 from sklearn.metrics import auc_score
 import numpy as np
 
-###########################
-# LOADING TRAINING DATA
-###########################
-
 trainfile = open('train.csv')
 header = trainfile.next().rstrip().split(',')
 
@@ -36,17 +32,10 @@ def transform_features(x):
 X_train = transform_features(X_train_A) - transform_features(X_train_B)
 model = linear_model.LogisticRegression(fit_intercept=False)
 model.fit(X_train,y_train)
-# compute AuC score on the training data (BTW this is kind of useless due to overfitting, but hey, this is only an example solution)
 p_train = model.predict_proba(X_train)
 p_train = p_train[:,1:2]
-print 'AuC score on training data:',auc_score(y_train,p_train.T)
-
-###########################
-# READING TEST DATA
-###########################
 
 testfile = open('test.csv')
-#ignore the test header
 testfile.next()
 
 X_test_A = []
@@ -62,17 +51,11 @@ testfile.close()
 X_test_A = np.array(X_test_A)
 X_test_B = np.array(X_test_B)
 
-# transform features in the same way as for training to ensure consistency
 X_test = transform_features(X_test_A) - transform_features(X_test_B)
-# compute probabilistic predictions
 p_test = model.predict_proba(X_test)
-#only need the probability of the 1 class
 p_test = p_test[:,1:2]
 
-###########################
-# WRITING SUBMISSION FILE
-###########################
-predfile = open('predictions.csv','w+')
+predfile = open('predictions.csv','w')
 
 print >>predfile,','.join(header)
 for line in np.concatenate((p_test,X_test_A,X_test_B),axis=1):
